b971470a53a9aae7ddf1120a81bd381d9b01e2e6
Dane Jordan <dane.e.jordan@gmail.com>
2016-07-27 12:29:28 -0700

    feat/ skeleton

---
+++ mobile/Trendwave/app/index.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+// Here we can use import statesments using the 
+// naming convention component.ios.js and component.android.js
+
+// import Component from  './components/component' }
+
+export default class Trendwave extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>TrendWave</Text>
+        {/*<Component>*/}
+        {/*<Component>*/}
+        {/*<Component>*/}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
