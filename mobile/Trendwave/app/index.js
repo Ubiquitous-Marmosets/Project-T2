461eddaa41882ff59d59060de9c8df811bbf567d
Dane Jordan <dane.e.jordan@gmail.com>
2016-07-29 22:42:59 -0700

    feat: sidebar connected to twitter fetch and refresh main page

--- mobile/Trendwave/app/index.js
+++ mobile/Trendwave/app/index.js
@@ -9,14 +9,18 @@ import TrendScore from './components/TrendScore';
 import EmotionalFeedback from './components/EmotionalFeedback';
 import Sentiment from './components/sentiment';
 import Menu from './components/Menu';
+import moment from 'moment';
+
 
 export default class Trendwave extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      popTweets: [],
-      popHeadlines: []
+      popTweets: ['schwag'],
+      selectedTrend: 'Stephen Curry',
+      trends:[],
+      menuOpen: false
     };
   }
 
@@ -28,15 +32,52 @@ export default class Trendwave extends Component {
     });
   }
 
+  fetchTrend(trend) {
+    this.setState({menuOpen: false, selectedTrend: trend});
+    // fetch('http://localhost:3000/grabTweets', {
+    //   method: "POST",
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({ q: trend })//q })
+    // })
+    // .then(res => res.json())
+    // .then(response => this.setState({popTweets: response, selectedTrend: trend}))
+    // .then(() =>)
+    // .catch(err => console.log('err:', err));
+    // 
+    fetch('http://localhost:3000/grabTopTweet', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ q: trend})
+    }).then(res => res.json())
+    .then(response => {
+      let firstTweet = `${response.firstUser}: ${response.firstTweet} \n ${moment(response.firstTweetTime).fromNow()}`;
+      let secondTweet = `${response.secondUser}: ${response.secondTweet} \n ${moment(response.secondTweetTime).fromNow()}`;
+      this.setState({
+        popTweets: {
+          firstTweet: firstTweet,
+          secondTweet: secondTweet
+        }
+      });
+    })
+    .catch(response => console.log('Top Tweet Grab Error:', response));
+  }
+
+
   render() {
-    const menu = <Menu trends={this.state.trends}/>;
+    const menu = <Menu isOpen={this.state.menuOpen} fetchTrend={this.fetchTrend.bind(this)} trends={this.state.trends}/>;
 
     return (
         <SideMenu menu={menu}>
         <StatusBar hidden= 'true' />
           <ScrollView style={styles.container}>
             <PieChart styles={styles.chart} />
-            <PopTweets popTweets={this.state.popTweets}/>
+            <PopTweets popTweets={this.state.popTweets} selectedTrend={this.state.selectedTrend}/>
             <PopHeadlines />
             <TrendScore />
             <EmotionalFeedback />
